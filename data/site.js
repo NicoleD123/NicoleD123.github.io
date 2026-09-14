05fe1f48df9585335e9db21c0647cbc5f3d3757f
Weichun Nicole Deng <ndeng01@stanford.edu>
2026-09-13 22:36:01 -0700

    roman numerals match theme title; capitalize department

--- data/site.js
+++ data/site.js
@@ -9,7 +9,7 @@ const SITE = {
   nameAlt: "邓维淳",
 
   role: "Research Coordinator & Lab Manager",
-  affiliation: "@spanlab, department of psychology",
+  affiliation: "@spanlab, Department of Psychology",
 
   // 侧边栏底部的基本信息。schoolUrl 填了的话，"Stanford University" 就会变成可点的链接。
   school: "Stanford University",
